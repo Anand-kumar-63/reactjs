d068e6ded942b4ab1849302f2f9300fb48aae1e9
Anand-kumar-63 <anandks8013@gmail.com>
2024-10-02 01:25:44 +0530

    added hooks(useState, useEffect)

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-#hello read me
+# exports
+There are two types of Exports:: 
+1> Default export => during import  => import cards from "./components/cards";
+2> Named export => during import => import {cards} from "./components/cards";
+
+# hooks function
+(normal js utility function)
+- usestate() 
+- useEffects()
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,70 @@
 // functional component of Body
 
 import reslist from "../utils/data";
-import Cards  from "./cards";
+import Cards from "./cards";
+import { useState,useeffect } from "react";
+import reslist from "../utils/data";
 
 const Body = () => {
+
+useeffect
+
+
+  // this is a Normal js variable
+  // let listofrestaurants = [
+  //   {
+  //     info: {
+  //       id: "150586",
+  //       name: "Accord International",
+  //       cloudinaryImageId: "ntcsuou2650qimnkrc3m",
+  //       locality: "Parasia Road",
+  //       areaName: "Vishnu Nagar",
+  //       costForTwo: "₹400 for two",
+  //       cuisines: ["North Indian", "South Indian", "Continental", "Beverages"],
+  //       avgRating: 4.3,
+  //       parentId: "26828",
+  //       totalRatingsString: "570",
+  //     },
+  //   },
+
+  //   {
+  //     info: {
+  //       id: "150584",
+  //       name: "Accord National",
+  //       cloudinaryImageId: "ntcsuou2650qimnkrc3m",
+  //       locality: "Parasia Road",
+  //       areaName: "Vishnu Nagar",
+  //       costForTwo: "₹200 for two",
+  //       cuisines: ["North Indian", "South Indian", "Continental", "Beverages"],
+  //       avgRating: 3.5,
+  //       parentId: "26828",
+  //       totalRatingsString: "570",
+  //     },
+  //   },
+  // ];
+
+  // state variable - super powerfull variable:
+  let [listofrestaurants, setlistofrestaurants] = useState(reslist);
   return (
     <div className="Body">
-      <div className="searchbar">Search</div>
+      {/* updating the list of restaurants by using filter those restaurants having rating greater than 4 on clicking the button::*/}
+      <button
+        className="filter_btn"
+        onClick={() => {
+          filteredlistofrestaurants = listofrestaurants.filter(
+            (res) => res.info.avgRating > 4
+          );
+          console.log(filteredlistofrestaurants);
+          // updating the list of restuarants::by passing the filtered values of restaurants by filter method::
+          setlistofrestaurants(filteredlistofrestaurants);
+        }}
+      >
+        Top Rated Restaurants
+      </button>
       <div className="res_cards">
-        {/* <Cards  resdata= {reslist[0]}/>
+        <div>
+          {" "}
+          {/* <Cards  resdata= {reslist[0]}/>
         <Cards  resdata = {reslist[1]}/>
         <Cards  resdata = {reslist[2]}/> 
         <Cards  resdata = {reslist[3]}/> 
@@ -16,14 +72,12 @@ const Body = () => {
         <Cards  resdata = {reslist[5]}/> 
         <Cards  resdata = {reslist[6]}/> 
         <Cards  resdata = {reslist[7]}/>  */}
+        </div>
+
         {/* now make it a simpler code by using js map function*/}
-        {
-        reslist.map((restaurant,index) => (<Cards 
-           key={index}
-           resdata={restaurant}/> ))
-
-        }
-
+        {listofrestaurants.map((restaurant, index) => (
+          <Cards key={index} resdata={restaurant} />
+        ))}
       </div>
     </div>
   );
